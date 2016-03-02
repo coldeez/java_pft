6020212bb7a316950549dfab7de191a44b25e906
coldeez <kbal@wshp258msk.corp.mango.ru>
2016-03-02 19:49:56 +0300

    Добавлены вспомогательные классы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by kbal on 02.03.2016.
+ */
+public class NavigationHelper {
+  private FirefoxDriver wd;
+
+  public NavigationHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  public void goToGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+}
