c6608a50076ece6778e63a04511b3c87466bfa45
coldeez <kbal@wshp258msk.corp.mango.ru>
2016-03-03 19:01:29 +0300

    Добавлен переход на страницу с контактами для класса создания контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,6 +8,7 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
+    app.getNavigationHelper().returnToHomePage();
     app.getContactHelper().initAccountCreation();
     app.getContactHelper().fillContactForm(new ContactData("test1", "test2", "testmail1@mail.ru", "City, street, flat", "testmail2@mail.ru", "+1234567890", "+9061234567"));
     app.getContactHelper().submitAccountCreation();
