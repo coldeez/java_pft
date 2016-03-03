c38a87a3b132f88887a0b1c6bd1630d53c945dfb
coldeez <kbal@wshp258msk.corp.mango.ru>
2016-03-03 18:59:56 +0300

    Добавлен переход на страницу с контактами и новый класс изменения группы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by kbal on 03.03.2016.
+ */
+public class GroupModificationTests extends TestBase {
+
+   @Test
+   public void testGroupModification(){
+     app.getNavigationHelper().goToGroupPage();
+     app.getGroupHelper().selectGroup();
+     app.getGroupHelper().initGroupModification();
+     app.getGroupHelper().fillGroupForm(new GroupData("test4", "test5", "test6"));
+     app.getGroupHelper().submitGroupModification();
+     app.getGroupHelper().returnToGroupPage();
+   }
+
+}
