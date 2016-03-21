c86c6c0cbd5286c9c395c51f3e40da6194555a27
coldeez <kbal@corp.mango.ru>
2016-03-21 16:20:58 +0300

    Номер группы для выбора передаются параметром

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -11,12 +11,12 @@ public class GroupModificationTests extends TestBase {
 
    @Test
    public void testGroupModification(){
-     int before = app.getGroupHelper().getGroupCount();
      app.getNavigationHelper().goToGroupPage();
-       if (! app.getGroupHelper().isThereAGroup()) { /*проверка на наличие хотя бы одной группы*/
+     int before = app.getGroupHelper().getGroupCount();
+     if (! app.getGroupHelper().isThereAGroup()) { /*проверка на наличие хотя бы одной группы*/
            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
        }
-     app.getGroupHelper().selectGroup();
+     app.getGroupHelper().selectGroup(before - 1);
      app.getGroupHelper().initGroupModification();
      app.getGroupHelper().fillGroupForm(new GroupData("test1", "test5", "test6"));
      app.getGroupHelper().submitGroupModification();
