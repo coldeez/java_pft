a939555e8a4df1d355a3d8b770504b5a5250f947
coldeez <coldywebdis@yandex.ru>
2016-03-22 20:48:25 +0300

    Добавлено предусловие для проверки при удалении контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 /**
  * Created by kbal on 03.03.2016.
  */
@@ -12,18 +14,24 @@ public class ContactDeletionTests extends TestBase {
   @Test
   public void testContactDeletion() {
   app.getContactHelper().goToHomePage();
-  int before = app.getContactHelper().getContactCount();
+    List<ContactData> before = app.getContactHelper().getContactList();
     if (! app.getContactHelper().isThereAContact()) { /*проверка на наличие хотя бы одного контакта*/
       app.getContactHelper().createGroup(new ContactData("test1", "test2", "testmail1@mail.ru", "City, street, flat", "testmail2@mail.ru", "+1234567890", "+9061234567", "test2"), true);
     }
-  app.getContactHelper().selectContact(before - 1);
+    if (before.size() != 0) {
+    app.getContactHelper().selectContact(before.size() - 1);
+    }
+    else app.getContactHelper().selectContact(before.size());
   app.getContactHelper().clickDeleteButton();
   app.navigationHelper.acceptAlert(app);
-  int after = app.getContactHelper().getContactCount();
-  if (before == 0) {
-      Assert.assertEquals(after, before);
+  app.getContactHelper().goToHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+  if (before.size() == 0) {
+    Assert.assertEquals(after.size(), before.size());
   }
-  else Assert.assertEquals(after, before - 1);
+  else Assert.assertEquals(after.size(), before.size() - 1);
+
+
 
 
   }
