78505eb4621fd11ebe7356b1392eab5fc2f81ffc
coldeez <coldywebdis@yandex.ru>
2016-03-24 21:38:32 +0300

    Revert "Добавлены проверки сравнения упорядоченных списков по все тесты для контактов"
    This reverts commit a3f9ffc91d13ed5f52d0efb03d13d38531449cff.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -26,6 +26,6 @@ public class ContactDeletionTests extends TestBase {
   List<ContactData> after = app.getContactHelper().getContactList();
   Assert.assertEquals(after.size(), before.size() - 1);
   before.remove(before.size() - 1);
-  Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Assert.assertEquals(after,before);
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -30,10 +29,7 @@ public class ContactModificationTests extends TestBase {
     Assert.assertEquals(after.size(), before.size());
     before.remove(before.size() - 1);
     before.add(contact);
-    Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(),c2.getId());
-    before.sort(byId);
-    after.sort(byId);
-    Assert.assertEquals(before, after);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
   }
 }
