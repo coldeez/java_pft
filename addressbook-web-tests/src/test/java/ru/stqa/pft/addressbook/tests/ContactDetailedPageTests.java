db20bb5fb1c9a57043f1dc1b0921fc1d4e37bac8
coldeez <kbal@corp.mango.ru>
2016-04-09 18:43:32 +0300

    Правка по 12 заданию

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDetailedPageTests.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by kbal on 09.04.2016.
+ */
+public class ContactDetailedPageTests extends TestBase {
+
+  @Test
+  private void 
+}
