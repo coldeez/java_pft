2eba015a8fc12e1c91fffe684568d590733f385d
coldeez <kbal@corp.mango.ru>
2016-04-19 20:57:35 +0300

    Добавлено сравнение списков групп из БД для теста модификации групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -24,6 +24,7 @@ public class ApplicationManager {
   public NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private String browser;
+  private DbHelper dbHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -34,6 +35,7 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader((new File(String.format("src/test/resources/%s.properties", target)))));
+    dbHelper = new DbHelper();
     if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
     } else if (browser.equals(BrowserType.CHROME)) {
@@ -66,5 +68,9 @@ public class ApplicationManager {
   public ContactHelper contact() {
     return contactHelper;
   }
+  public DbHelper db() {
+    return dbHelper;
+  }
+
 
 }
