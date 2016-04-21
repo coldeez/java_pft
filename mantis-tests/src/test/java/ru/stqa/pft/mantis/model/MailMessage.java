1d4b9dcf7f6a09f3697f140149f4c6b2850ff976
coldeez <kbal@corp.mango.ru>
2016-04-21 21:56:15 +0300

    Добавление чтение почты (fail)

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/model/MailMessage.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.mantis.model;
+
+/**
+ * Created by kbal on 21.04.2016.
+ */
+public class MailMessage {
+
+  public String to;
+  public String text;
+
+  public MailMessage(String to, String test) {
+    this.to = to;
+    this.text = text;
+  }
+}
