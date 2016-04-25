9e94ccfba6eaa1a799cc10d20c08ddd88e39e0c7
coldeez <kbal@corp.mango.ru>
2016-04-25 12:52:46 +0300

    Добавлена генерация отчетов allure со скриншотами при падении тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -61,7 +61,7 @@ public class GroupCreationTests extends TestBase {
     app.goTo().groupPage();
     Groups before = app.db().groups();
     app.group().create(group);
-    assertThat(app.group().count(),equalTo(before.size() + 1));
+    assertThat(app.group().count()+1,equalTo(before.size() + 1));
     Groups after = app.db().groups();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt()))));
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -23,13 +23,14 @@ public class GroupModificationTests extends TestBase {
 
    @Test
    public void testGroupModification(){
+
      Groups before = app.db().groups();
      GroupData modifiedGroup = before.iterator().next();
      GroupData group = new GroupData().
              withId(modifiedGroup.getId()).withName("test2").withHeader("test5").withFooter("test6");
      app.goTo().groupPage();
      app.group().modify(group);
-     assertThat(app.group().count(),equalTo(before.size()));
+     assertThat(app.group().count() ,equalTo(before.size()));
      Groups after = app.db().groups();
      assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
      verifyGroupListInUI();
